f7bde5c8b1a2379ce8c8c529d30c00222ce0cecf
Ed Prats <edprats@gmail.com>
2016-01-07 22:02:09 -0800

    first commit of basic counter increment with UI created via view controller

--- Click Counter/ViewController.swift
+++ Click Counter/ViewController.swift
@@ -9,16 +9,74 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count = 0
+    var label: UILabel!
+    var bgColor = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // create a label
+        label = UILabel()
+        // position label
+        label.frame = CGRectMake(150, 150, 60, 60)
+        label.text = "0"
+        // add label to the main view
+        self.view.addSubview(label)
+        
+        // create an increment button
+        let buttonIncrement = UIButton()
+        buttonIncrement.frame = CGRectMake(150, 250, 100, 60)
+        buttonIncrement.setTitle("Increment", forState: .Normal)
+        buttonIncrement.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        // add button to the main view
+        self.view.addSubview(buttonIncrement)
+        
+        buttonIncrement.addTarget(self, action: "incrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        // create an decrease button
+        let buttonDecrement = UIButton()
+        buttonDecrement.frame = CGRectMake(150, 300, 100, 60)
+        buttonDecrement.setTitle("Decrement", forState: .Normal)
+        buttonDecrement.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        // add button to the main view
+        self.view.addSubview(buttonDecrement)
+        
+        buttonDecrement.addTarget(self, action: "decrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        // create button to change view background color
+        let buttonBackgroundToggle = UIButton()
+        buttonBackgroundToggle.frame = CGRectMake(150, 350, 100, 60)
+        buttonBackgroundToggle.setTitle("BG Toggle", forState: .Normal)
+        buttonBackgroundToggle.setTitleColor(UIColor.redColor(), forState: .Normal)
+        
+        self.view.addSubview(buttonBackgroundToggle)
+        
+        buttonBackgroundToggle.addTarget(self, action: "toggleBackgroundColor", forControlEvents: UIControlEvents.TouchUpInside)
+        
+    }
+    
+    func incrementCount() {
+        self.count++
+        self.label.text = "\(self.count)"
+    }
+    
+    func decrementCount() {
+        self.count--
+        self.label.text = "\(self.count)"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func toggleBackgroundColor() {
+        if bgColor == 0 {
+            self.view.backgroundColor = UIColor.greenColor()
+            bgColor++
+        } else {
+            self.view.backgroundColor = UIColor.whiteColor()
+            bgColor--
+        }
     }
+
 
 
 }
